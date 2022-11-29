0e7840282901bdc697167802941cab862cada68e
simDev1234 <simDev1234@gmail.com>
2022-11-29 21:06:39 +0900

    chore: BASE-MSA-ENV 기본 환경 설정
    - 최상위 build.gradle을 통해 하위 모듈에 공통으로 들어가는 라이브러리를 추가했습니다.
      (JPA, Lombok, Swagger2, DB driver(실섭 MySQL, 테섭 H2)
    - 각각의 회원, 상품 모듈에 Swagger2Config 클래스를 추가했습니다.

---
+++ src/main/java/com/teamzero/zeromall/ZeromallApplication.java
@@ -0,0 +1,13 @@
+package com.teamzero.zeromall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeromallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeromallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamzero/zeromall/ZeromallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamzero.zeromall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeromallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
