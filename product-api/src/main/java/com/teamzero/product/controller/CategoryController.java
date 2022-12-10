cd50f5042afcb41219b6805e3456bf6803821665
jeralzzang <79639143+jeralzzang@users.noreply.github.com>
2022-12-10 22:50:26 +0900

    feat:상품 카테고리 추가, 수정, 삭제, 조회 (#12)
    * feat:상품 카테고리 추가, 수정, 삭제, 조회

---
+++ product-api/src/main/java/com/teamzero/product/controller/CategoryController.java
@@ -0,0 +1,65 @@
+package com.teamzero.product.controller;
+
+import com.teamzero.product.domain.model.CategoryRegister;
+import com.teamzero.product.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/cat")
+public class CategoryController {
+  private final CategoryService categoryService;
+
+  /**
+   * 카테고리 등록
+   * @return
+   */
+  @PostMapping("/register")
+  public ResponseEntity<?> categoryRegister(@RequestBody CategoryRegister request){
+
+    return ResponseEntity.ok(categoryService.categoryRegister(request));
+  }
+
+  /**
+   * 조건 별 카테고리 조회
+   * 1. 카테고리Id, 카테고리명, 카테고리 타입으로 조회 가능
+   */
+  @PostMapping("/find")
+  public ResponseEntity<?> categoryFind(@RequestBody CategoryRegister request){
+
+    return ResponseEntity.ok(categoryService.categoryFind(request));
+  }
+
+  /**
+   * 카테고리 전체조회
+   */
+  @PostMapping("/findAll")
+  public ResponseEntity<?> categoryFindAll(){
+    return ResponseEntity.ok(categoryService.categoryFindAll());
+  }
+
+  /**
+   * 카테고리 삭제
+   * 1. 카테고리 코드, 카테고리 타입 으로 삭제가능
+   * 2. 하위카테고리가 있을경우 오류응답
+   */
+  @PostMapping("/delete")
+  public ResponseEntity<?> categoryDelete(@RequestBody CategoryRegister request){
+
+    return ResponseEntity.ok(categoryService.categoryDelete(request));
+  }
+  /**
+   * 카테고리명 수정
+   * 1.카테고리 코드로 수정 가능
+   */
+  @PostMapping("/modify")
+  public ResponseEntity<?> categoryModify(@RequestBody CategoryRegister request){
+
+    return ResponseEntity.ok(categoryService.categoryModify(request));
+  }
+}
---
+++ product-api/src/main/java/com/teamzero/product/domain/repository/CategoryRepository.java
@@ -0,0 +1,64 @@
+package com.teamzero.product.domain.repository;
+
+import com.teamzero.product.domain.model.CategoryEntity;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<CategoryEntity, String> {
+  boolean existsByCatName(String catName);
+  int countByCatIdLike(String catId);
+  boolean existsByCatId(String catId);
+  Optional<CategoryEntity> findByCatId(String catId);
+
+  /**
+   * 카테고리 조회
+   * @param catId
+   * @param catName
+   * @return
+   */
+  List<CategoryEntity> findAllByCatIdAndCatName(String catId, String catName);
+  List<CategoryEntity> findAllByCatNameAndCatIdNotLike
+      (String catName, String catId);
+  List<CategoryEntity> findAllByCatNameAndCatIdLike
+      (String catName, String catId);
+
+  List<CategoryEntity> findAllByCatId(String catId);
+  List<CategoryEntity> findAllByCatName(String catName);
+  List<CategoryEntity> findAllByCatIdLike(String catId);
+  List<CategoryEntity> findAllByCatIdNotLike(String catId);
+  @Query("select cat "
+      + "from CategoryEntity as cat "
+      + "where cat.catId like :catId and cat.catId not like :notCatId")
+  List<CategoryEntity> findAllByCatIdLikeCatIdNotLike(String catId, String notCatId);
+
+  @Query("select cat "
+      + "from CategoryEntity as cat "
+      + "where cat.catName = :catName "
+      + "and cat.catId like :catId and cat.catId not like :notCatId")
+  List<CategoryEntity> findAllByCatNameAndCatIdLikeCatIdNotLike
+      (String catName, String catId, String notCatId);
+
+  /**
+   * 카테고리 타입별 최대카테고리값 가져오기
+   * @return
+   */
+  @Query("select max(cat.catId) as maxCatId "
+      + "from CategoryEntity as cat where cat.catId like '%000000'")
+  String maxByCatIdAtype();
+
+  @Query(value = "select max(cat.catId) as maxCatId "
+      + "from CategoryEntity as cat "
+      + "where cat.catId like concat(substring(:parentId,1,3),'%')")
+  String maxByCatIdBtype(@Param("parentId") String parentId);
+
+  @Query(value = "select max(cat.catId) as maxCatId "
+      + "from CategoryEntity as cat "
+      + "where cat.catId like concat(substring(:parentId,1,6),'%')")
+  String maxByCatIdCtype(String parentId);
+
+}
