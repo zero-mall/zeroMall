93f89970971db6216e6f7abc10c1a2e9dc6862c1
simDev1234 <simDev1234@gmail.com>
2022-12-18 02:37:09 +0900

    feat: [ERROR-FIX] product-api가 user-api의 repository bean을 읽지 못하는 현상 해결

--- product-api/src/main/java/com/teamzero/product/ProductApplication.java
+++ product-api/src/main/java/com/teamzero/product/ProductApplication.java
@@ -2,14 +2,14 @@ package com.teamzero.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaAuditing
-@EnableJpaRepositories
 @EnableFeignClients
+@EnableJpaRepositories(basePackages = {"com.teamzero.product.domain.repository", "com.teamzero.member.domain.repository"})
+@EntityScan(basePackages = {"com.teamzero.product.domain.model", "com.teamzero.member.domain.model"})
 public class ProductApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProductApplication.class, args);
