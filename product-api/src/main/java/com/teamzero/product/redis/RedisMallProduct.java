b20bbcbb4f8e2a559bc1266e4187475640f04abc
jeralzzang <jeralzzang@naver.com>
2022-12-24 16:25:18 +0900

    feat: 쇼핑몰 상품 스케쥴러 redis환경 추가

---
+++ product-api/src/main/java/com/teamzero/product/redis/RedisMallProduct.java
@@ -0,0 +1,21 @@
+package com.teamzero.product.redis;
+
+import com.teamzero.product.domain.dto.product.MallProductSearch.Response;
+import com.teamzero.product.domain.model.constants.CacheKey;
+import java.util.HashMap;
+import java.util.List;
+import javax.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.redis.core.RedisHash;
+
+@Data
+@NoArgsConstructor
+@RedisHash(CacheKey.MALLPRODUCT_LIST)
+public class RedisMallProduct {
+  @Id
+  private String id;
+
+  private HashMap<String, List<Response>> productMap = new HashMap<>();
+
+}
