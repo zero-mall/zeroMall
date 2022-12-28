13f430d4a0725bc44b10adf4232e9380d85d1316
jeralzzang <jeralzzang@naver.com>
2022-12-28 23:52:33 +0900

    feat: 테스트 데이터 insert문 추가

--- product-api/src/main/resources/data/mall.sql
+++ product-api/src/main/resources/data/mall.sql
@@ -1,6 +1,6 @@
-insert into mall (mall_id, name) values (1, '11번가') on duplicate key update mall_id = values(mall_id);
-insert into mall (mall_id, name) values (2, 'G마켓') on duplicate key update mall_id = values(mall_id);
-insert into mall (mall_id, name) values (3, '티몬') on duplicate key update mall_id = values(mall_id);
-insert into mall (mall_id, name) values (4, 'AK몰') on duplicate key update mall_id = values(mall_id);
-insert into mall (mall_id, name) values (5, '다나와') on duplicate key update mall_id = values(mall_id);
-insert into mall (mall_id, name) values (6, '위메프') on duplicate key update mall_id = values(mall_id);
+insert into mall (mall_id, name,scheduler_yn) values (1, '11번가', false) on duplicate key update mall_id = values(mall_id);
+insert into mall (mall_id, name,scheduler_yn) values (2, 'G마켓', false) on duplicate key update mall_id = values(mall_id);
+insert into mall (mall_id, name,scheduler_yn) values (3, '티몬', false) on duplicate key update mall_id = values(mall_id);
+insert into mall (mall_id, name,scheduler_yn) values (4, 'AK몰', false) on duplicate key update mall_id = values(mall_id);
+insert into mall (mall_id, name,scheduler_yn) values (5, '다나와', false) on duplicate key update mall_id = values(mall_id);
+insert into mall (mall_id, name,scheduler_yn) values (6, '위메프', false) on duplicate key update mall_id = values(mall_id);
